42307447145cd41c17167ec0c808ffbc7fa54ccf
kuldeeppushpad-creator <kuldeeppushpad440@gmail.com>
2026-02-15 22:41:04 +0530

    Add files via upload

---
+++ action.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" id="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+
+    <?php
+       
+<a href="https://docs.google.com/spreadsheets/d/1n3VaH8YynXCf0NurcS-ZjovthwJwq7cKcB6cCYkWLVI/edit?usp=drivesdk"></a>
+	
+    </body>
+</html>		
